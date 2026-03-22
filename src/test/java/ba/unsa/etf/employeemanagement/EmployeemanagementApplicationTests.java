fedc019aa1f560c023c8ed5ce35bbe6937b29a7d
amarderschrecklicher <atahirovic3@etf.unsa.ba>
2026-03-22 12:29:47 +0100

    initial commit

---
+++ src/test/java/ba/unsa/etf/employeemanagement/EmployeemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
